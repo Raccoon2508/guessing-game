616ee4102b0d4c5274c599840f88f6c89b03e3b8
Raccoon2508 <ridge2006@mail.ru>
2019-10-09 23:58:28 +0300

    Ready solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,41 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this._curNum=null;
+      this._minNum=null;
+      this._maxNum=null;
+      this._numArr=[];
+      this.midInd=null;
+      
+      }
 
     setRange(min, max) {
-
+      this._numArr=[];
+      for(let i=min; i<=max;i++){
+        this._numArr.push(i);}
+      
+      this._minNum=min; 
+      this._maxNum=max;
+      
+     
+        
     }
 
     guess() {
+      this.midInd=Math.floor((this._numArr.length)/2);
+      return this._numArr[this.midInd]; 
 
     }
 
     lower() {
+      
+      this.setRange(this._minNum, this._numArr[this.midInd]);
+     
+      
 
     }
 
     greater() {
+     this.setRange(this._numArr[this.midInd],this._maxNum);
 
     }
 }
